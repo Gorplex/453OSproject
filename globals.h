8fd608ccc8dea8a0a66935e09526b310b4f33ffb
root <root@MSI-John.localdomain>
2018-04-17 14:19:09 -0700

    lab1 files

---
+++ globals.h
@@ -0,0 +1,14 @@
+//Global defines
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+//place defines and prototypes here
+#include <stdint.h>
+#include <util/delay.h>
+#include "serial.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+#endif
